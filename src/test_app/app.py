55761cf8f055481ea9897f2d21ee4f5a3cd9a032
hari2357s <harikarthick289@gmail.com>
2026-01-16 10:05:31 +0530

    testing

--- src/test_app/app.py
+++ src/test_app/app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from test_app.services.test_service import return_hello
+from test_app.common.container import test_container
 
 st.header("Hello World!")
 st.write(return_hello())
+st.text(test_container())
---
+++ src/test_app/common/container.py
@@ -0,0 +1,4 @@
+
+
+def test_container():
+    return "container working"
